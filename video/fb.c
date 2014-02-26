05fac7701ddec9125a16a7653cb53eafcf09df21
Strake <strake888@gmail.com>
2014-02-26 09:20:29 -0500

    mmap returns nonzero on failure, which would make sense if it weren't a pointer

--- video/fb.c
+++ video/fb.c
@@ -14,7 +14,7 @@ int getVi () {
 	size_t fbSize = fbfi.line_length * fbvi.yres;
 	if (ioctl (fbfd, FBIOGET_VSCREENINFO, &fbvi) < 0) return (-1);
 	fb = mremap (fb, fbSize, fbfi.line_length * fbvi.yres, MREMAP_MAYMOVE);
-	if (!fb) return (-1);
+	if (fb == MAP_FAILED) return (-1);
 	return 0;
 }
 
@@ -22,7 +22,7 @@ int putVi () {
 	size_t fbSize = fbfi.line_length * fbvi.yres;
 	if (ioctl (fbfd, FBIOPUT_VSCREENINFO, &fbvi) < 0) return (-1);
 	fb = mremap (fb, fbSize, fbfi.line_length * fbvi.yres, MREMAP_MAYMOVE);
-	if (!fb) return (-1);
+	if (fb == MAP_FAILED) return (-1);
 	return 0;
 }
 
@@ -37,7 +37,7 @@ int initVideo () {
 		return (-1);
 	}
 	fb = mmap (0, fbfi.line_length * fbvi.yres, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
-	if (!fb) return (-1);
+	if (fb == MAP_FAILED) return (-1);
 	return 0;
 }
 
